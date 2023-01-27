e0a93e6a13618312de15e5153576ada7cb53de3c
Penmaster <96364366+Menkallinan@users.noreply.github.com>
2023-01-26 22:33:35 -0300

    Add files via upload
    Resto 1.0

---
+++ bee_3091.cpp
@@ -0,0 +1,24 @@
+/*William, John é um aluno vidrado por matemática, mas poucas pessoas sabem disso. Certo dia, um de seus colegas de classe achando que John não sabia conceitos básicos da matemática o desafiou a realizar um simples cálculo que era: Calcular o resto da divisão de A ÷ B
+
+Como John é muito tímido e vocês são melhores amigos, ele te informou qual era a resposta para o problema e pediu para que você passasse tal informação para esse colega que o desafiou. Você pode fazer isso ?
+
+Dado dois inteiros A e B, diga qual o resto da divisão de A ÷ B que John passou para você.
+
+Entrada
+A entrada contém dois inteiros A e B (1 <= A, B <= 100000).
+
+Saída
+A saída contém um único inteiro que é o resto da divisão de A ÷ B.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    cout<<a%b<<endl;
+    return 0;
+}
